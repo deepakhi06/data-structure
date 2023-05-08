5ca93aa45da7aec7f39d6267ed6452011b20f196
Deepak <deep.abhi06@gmail.com>
2023-05-08 21:49:44 +0530

    adding String reverse program

--- src/string/ReverseString.java
+++ src/string/ReverseString.java
@@ -1,19 +1,26 @@
-package string;
+package programmingq;
+
 public class ReverseString {
+	public static void main(String[] args) {
+		//By using charAt(index) method
+		String s1="Hello",rev="";
+		//olleH charAt(4)--o
+		//      charAt(3)--l
+		//      charAt(2)--l
+		//      charAt(1)--e
+		//      charAt(0)--H
+		for(int i=s1.length()-1;i>=0;i--) {
+			rev=rev+s1.charAt(i);
+		}
+		
+		System.out.println("Reverse string is :"+rev);
+		
+		//By using StringBuilder and StringBuffer //s1.reverse()
+		StringBuilder strBuilder = new StringBuilder(s1);
+		strBuilder.reverse();
+		
+		System.out.println("=== String revers by StringBuilder==="+strBuilder.toString());
+		
+	}
 
-    public String reverse(String input){
-        if(input.isEmpty())
-            return input;
-        //else put the character at the beginning to the end
-        //make a recursive call
-        return reverse(input.substring(1))+input.charAt(0);
-    }
-
-    public static void main(String[] args) {
-        ReverseString r = new ReverseString();
-        String input = "deepak";
-        String reversedString = r.reverse(input);
-        System.out.println("Original String: " + input);
-        System.out.println("Reversed String: " + reversedString);
-    }
 }
